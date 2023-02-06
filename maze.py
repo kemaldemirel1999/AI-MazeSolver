da6f0763db5ede65fb3e75d607459e1f60b59473
Kemal Demirel <kmldemirel99@gmail.com>
2023-02-06 16:57:57 +0300

    Functions added

--- maze.py
+++ maze.py
@@ -1,4 +1,18 @@
-from pyamaze import maze
-m=maze(5,5)
-m.CreateMaze()
-m.run()
+from queue import PriorityQueue
+
+def read_maze_from_text_file():
+    None
+
+def read_maze_from_jpg():
+    None
+    
+def a_start():
+    None
+    
+def get_difference(first_cell, second_cell):
+    x1, y1 = first_cell
+    x2, y2 = second_cell
+    return abs(x1-x2) + abs(y1-y2)
+
+
+

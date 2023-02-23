14c3990c6aae8bde4c4a9fa85ee75ccb1483fee7
Kemal Demirel <kmldemirel99@gmail.com>
2023-02-23 16:28:26 +0300

    start and goal cell added

--- jpgreader.py
+++ jpgreader.py
@@ -1,3 +1,3 @@
 import pywhatkit
 
-pywhatkit.image_to_ascii_art('maze.jpg','maze')
+pywhatkit.image_to_ascii_art('try.png','maze3')
--- maze.py
+++ maze.py
@@ -6,11 +6,12 @@ class Maze:
         self.grid = []
         self.maze_map = {}
         maze = self.read_maze_from_text_file()
-        maze = [[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1],[1,1,1,1,1]]
+        maze = [[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1]]
         path = self.a_star_algorithm(maze)
         
     def read_maze_from_text_file(self):
-        return None
+        with open('maze.txt') as f:
+            [print(line) for line in f.readlines()]
 
     def read_maze_from_jpg(self):
         None
@@ -22,7 +23,7 @@ class Maze:
                 self.maze_map[j,i]={'E':0,'W':0,'N':0,'S':0}
         return self.grid 
     
-    def diff(self,first_cell, second_cell):
+    def h_diff(self,first_cell, second_cell):
         x1, y1 = first_cell
         x2, y2 = second_cell
         return abs(x1-x2) + abs(y1-y2)
@@ -34,21 +35,31 @@ class Maze:
         None
         
     def a_star_algorithm(self,maze):
+        # Start point is given
         start = (5, 5)
+        
+        # G score array is created and start point's g_score added as 0
         g_score = {cell:float('inf') for cell in self.maze_grid(maze)}
         g_score[start]=0
+        
+        # Goal cell is defined
+        goal = (1,1)
+        
+        # F score array is created and start point's g_score added as 0
         f_score = {cell:float('inf') for cell in self.maze_grid(maze)}
-        f_score[start] = self.diff(start,(1,1))
+        f_score[start] = self.h_diff(start,goal)
         
         open=PriorityQueue()
-        open.put( (self.diff(start, (1,1)), self.diff(start, (1,1)), start) )
+        open.put( (self.h_diff(start, goal), self.h_diff(start, goal), start) )
         aPath = {}
+        
+        
         while not open.empty():
             currCell = open.get()[2]
-            if currCell == (1,1):
+            
+            if currCell == goal:
                 break
             for d in 'ESNW':
-                print(currCell,"  ", d)
                 if self.maze_map[currCell][d] == True:
                     if d=='E':
                         childCell = (currCell[0], currCell[1]+1)
@@ -58,17 +69,18 @@ class Maze:
                         childCell = (currCell[0]-1, currCell[1])
                     if d=='S':
                         childCell = (currCell[0]+1, currCell[1])
+
                     temp_g_score = g_score[currCell]+1
-                    temp_f_score = temp_g_score + self.diff(childCell,(1,1))
+                    temp_f_score = temp_g_score + self.h_diff(childCell, goal)
 
                     if temp_f_score < f_score[childCell]:
                         g_score[childCell] = temp_g_score
                         f_score[childCell] = temp_f_score
-                        open.put((temp_f_score, self.diff(childCell, (1,1)), childCell))
+                        open.put((temp_f_score, self.h_diff(childCell, goal), childCell))
                         aPath[childCell] = currCell
-                        
+
         fwdPath = {}
-        cell=(1,1)
+        cell = goal
         while cell != start:
             fwdPath[aPath[cell]] = cell
             cell = aPath[cell]

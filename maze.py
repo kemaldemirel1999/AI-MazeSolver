d5ae68f37bc04848634c4bcda606869da6256387
Kemal Demirel <kmldemirel99@gmail.com>
2023-03-22 15:41:07 +0300

    aStart BugFix

--- maze.py
+++ maze.py
@@ -133,8 +133,6 @@ class Maze:
         
         while not open.empty():
             currCell = open.get()[2]
-            if currCell == goal:
-                break
             for d in 'ESNW':
                 if self.maze_map[currCell][d] == True:
                     if d=='E':
@@ -154,6 +152,33 @@ class Maze:
                         f_score[childCell] = temp_f_score
                         open.put((temp_f_score, self.h_diff(childCell, goal), childCell))
                         aPath[childCell] = currCell
+        ####
+        
+        
+        if not open.empty():
+            currCell = open.get()[2]
+            if currCell == goal:
+                for d in 'ESNW':
+                    if self.maze_map[currCell][d] == True:
+                        if d=='E':
+                            childCell = (currCell[0], currCell[1]+1)
+                        if d=='W':
+                            childCell = (currCell[0], currCell[1]-1)
+                        if d=='N':
+                            childCell = (currCell[0]-1, currCell[1])
+                        if d=='S':
+                            childCell = (currCell[0]+1, currCell[1])
+
+                        temp_g_score = g_score[currCell]+1
+                        temp_f_score = temp_g_score + self.h_diff(childCell, goal)
+
+                        if temp_f_score < f_score[childCell]:
+                            g_score[childCell] = temp_g_score
+                            f_score[childCell] = temp_f_score
+                            open.put((temp_f_score, self.h_diff(childCell, goal), childCell))
+                            aPath[childCell] = currCell
+        
+        ####
         fwdPath = {}
         cell = goal
         while cell != start:
